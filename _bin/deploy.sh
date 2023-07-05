bcb4042d0e40ee6b57d2f7d778b9711eadfd40dd
Oliver Lau <oliver@ersatzworld.net>
2023-07-05 21:56:19 +0200

    Code gets minified before deployment.

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -2,4 +2,18 @@
 
 source .env
 
-rsync -rav src/* $REMOTE
+mkdir -p deploy
+
+rsync -rav --delete src/static deploy
+
+for HTMLFILE in index.html editor.html
+do
+    jsmin <src/$HTMLFILE >deploy/$HTMLFILE
+done
+
+for JSFILE in index.js editor.js
+do
+    uglifyjs --compress --mangle < src/$JSFILE > deploy/$JSFILE
+done
+
+rsync -rav --delete deploy/* $REMOTE
